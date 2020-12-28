9d4a28ebcc4d10d12ab27b0daebccad2691be253
Leon Tan <leon.arian.tan@gmail.com>
2020-12-28 22:17:31 +0100

    Setup yew app

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,33 @@
+#![recursion_limit = "256"]
+
+use yew::prelude::*;
+
+pub struct Model {}
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                {"Audio FP"}
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
